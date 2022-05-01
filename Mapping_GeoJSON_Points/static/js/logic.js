af1397f269c920f70947504141ca2a66fc830500
Jack Petrella <jackpetrella@yahoo.com>
2022-05-01 13:56:48 -0400

    Completed Mapping_GeoJSON_Points

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,33 +2,41 @@
 console.log("working");
 
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the line
+// Add GeoJSON data.
 
-let line = [
+let sanFranAirport = 
 
-    [37.6213, -122.3790],
+{"type": "FeatureCollection", "features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-    [30.1975, -97.6664],
-
-    [43.6777, -79.6248],
-
-    [40.6413, -73.7781]
-];
-
-// Create a polyline using the line coordinates and make the line yellow
-
-L.polyline(line, {
-    color: "blue",
-    weight: 4,
-    opacity: 0.5,
-    dashArray: "8"
+// Grabbing our GeoJSON data
+L.geoJSON(sanFranAirport,{
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup();
+    }
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     
     maxZoom: 18,
